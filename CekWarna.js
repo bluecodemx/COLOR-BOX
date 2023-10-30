4d538551589270c93638191287d50cb26b0f287b
bluecodemx <hizashi27@gmail.com>
2023-10-30 17:03:29 +0700

    first commit

---
+++ CekWarna.js
@@ -0,0 +1,14 @@
+// function change Color
+function myBoxPink (){
+    document.getElementById('box').style.backgroundColor=('#d63384')
+    document.getElementById('text').innerHTML='PINK COLOR'
+}
+function myBoxRed () {
+    document.getElementById('box').style.backgroundColor = ('#ff0000');
+    document.getElementById('text').innerHTML='RED COLOR'
+}
+function myBoxWhite(){
+    document.getElementById('box').style.backgroundColor = ('white');
+    document.getElementById('text').innerHTML='WHITE COLOR'
+}
+
